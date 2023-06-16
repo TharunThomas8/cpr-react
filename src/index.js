fcbf903928add8667235e31e32b265e165c9aaf1
Tharun <tharunthomas8@gmail.com>
2023-06-16 21:45:27 +0100

    button and screen

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Screen1 from './Screen1';
+
+const App = () => {
+  const [showScreen1, setShowScreen1] = React.useState(false);
+
+  const handleButtonClick = () => {
+    setShowScreen1(prevShowScreen1 => !prevShowScreen1);
+  };
+
+  return (
+    <div>
+      {!showScreen1 && (
+        <button onClick={handleButtonClick}>Go to Screen1</button>
+      )}
+      {showScreen1 && (
+        <div>
+          <button onClick={handleButtonClick}>Go back</button>
+          <Screen1 />
+        </div>
+      )}
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
-);
+root.render(<App />);
