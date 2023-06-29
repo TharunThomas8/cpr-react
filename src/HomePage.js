1888f838a93450f0b6680ce54c1e7405b335063e
Tharun <tharunthomas8@gmail.com>
2023-06-29 18:14:05 +0100

    options

--- src/HomePage.js
+++ src/HomePage.js
@@ -20,8 +20,16 @@ const HomePage = () => {
   const fetchData = () => {
     if (userId) {
       // Make Axios GET request with the user ID
+      const options = {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Basic <authString>',
+          'Access-Control-Allow-Origin': '*',
+        }
+      };
 
-      axios.get( '/get-last/' + userId)
+
+      axios.get('/get-last/' + userId, options)
         .then(response => {
           const responseData = response.data;
           console.log(responseData);
