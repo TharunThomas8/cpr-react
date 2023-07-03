8c2850168516afd4bb7f611d6c2fb87f651f291c
Tharun <tharunthomas8@gmail.com>
2023-07-03 21:21:44 +0100

    api string update

--- src/Screen2.js
+++ src/Screen2.js
@@ -21,7 +21,8 @@ const Screen2 = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(api_base+'get-user-data/${userId}');
+        console.log(userId);
+        const response = await axios.get(api_base+'get-user-data/'+userId);
         const responseData = response.data;
         if (responseData.success) {
           setUserData(responseData.data);
