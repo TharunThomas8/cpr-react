3747e1a1678475edf60352f4e5d1df9b7778404e
Tharun <tharunthomas8@gmail.com>
2023-07-12 12:02:53 +0100

    config :(

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 
 const api_base = "https://cpr-backend.vercel.app/";
 const page_base = "https://cpr-react.vercel.app/";
-const duration = 100000
+const duration = 60000
 
 // const api_base = "http://localhost:5000/";
 // const page_base = "http://localhost:3000/";
